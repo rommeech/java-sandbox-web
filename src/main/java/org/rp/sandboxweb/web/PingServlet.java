a2d669520903bfc766c1901c727355d0877407ce
Roman Parshin <parshin@smartmediastar.com>
2018-04-17 14:04:04 +0200

    Log4j2 config added

--- src/main/java/org/rp/sandboxweb/web/PingServlet.java
+++ src/main/java/org/rp/sandboxweb/web/PingServlet.java
@@ -7,10 +7,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class PingServlet extends HttpServlet {
+
+    private static Logger logger;
+
+    @Override
+    public void init() throws ServletException {
+        logger = LogManager.getLogger(PingServlet.class);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("doGet called");
         resp.setContentType("text/plain, charset=UTF-8");
         resp.setStatus(HttpServletResponse.SC_OK);
         PrintWriter writer = resp.getWriter();
@@ -19,6 +30,7 @@ public class PingServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        logger.error("doPost called");
         resp.setContentType("text/html, charset=UTF-8");
         resp.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
         PrintWriter writer = resp.getWriter();
